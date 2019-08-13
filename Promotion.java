aecf56fe5f9f87a4a5456a74ee9a6d8aaf52f103
HarideVos <53867807+HarideVos@users.noreply.github.com>
2019-08-12 20:19:55 -0400

    Add files via upload

---
+++ Promotion.java
@@ -0,0 +1,12 @@
+package fourpointtwo;
+
+public class Promotion {
+
+		public static void main(String[] args) {
+			int a = 7;
+			System.out.println("Integer: " + a);
+			float b = a;
+			System.out.println("Float b = i: " + b);
+		}
+	
+}
